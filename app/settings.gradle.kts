cb1f907373992c07d7849bdeeb58c10a04884286
amlord78 <88088117+amlord78@users.noreply.github.com>
2026-01-19 11:13:28 +0530

    Create settings.gradle.kts

---
+++ app/settings.gradle.kts
@@ -0,0 +1,18 @@
+cat > settings.gradle.kts <<'EOF'
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "chatapps"
+include(":app")
+EOF
